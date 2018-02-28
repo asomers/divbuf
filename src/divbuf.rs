ddcf72cdb795d09c7068f4f3fca1a0f5cde23583
Alan Somers <asomers@gmail.com>
2018-02-27 17:43:56 -0700

    Fully implemented all classes
    Removed a few prototypes that can't be implemented, like
    DivBufMut::freeze, or that conflict with the expected usage, like
    DivBufShared::from('static [u8])
    
    Full test coverage.

--- src/divbuf.rs
+++ src/divbuf.rs
@@ -1,7 +1,7 @@
 // vim: tw=80
 #![allow(unused)]
 
-use std::{hash, mem, ops, ptr};
+use std::{cmp, hash, mem, ops, ptr, slice};
 use std::sync::atomic::{AtomicBool, AtomicUsize};
 use std::sync::atomic::Ordering::{Relaxed, Acquire, Release, AcqRel};
 
@@ -20,32 +20,30 @@ struct Inner {
 #[derive(Debug)]
 pub struct DivBufShared {
     inner: *mut Inner,
-    //writing: AtomicBool
-    //readers: AtomicUsize,
-    //writers: AtomicUsize,
 }
 
 #[derive(Debug)]
 pub struct DivBuf {
     inner: *mut Inner,
-    ptr: *mut u8,
+    // In the future, consider optimizing by replacing begin with a pointer
+    begin: usize,
     len: usize,
 }
 
 #[derive(Debug)]
 pub struct DivBufMut {
     inner: *mut Inner,
-    ptr: *mut u8,
+    // In the future, consider optimizing by replacing begin with a pointer
+    begin: usize,
     len: usize,
 }
 
 impl DivBufShared {
     pub fn capacity(&self) -> usize {
-        unimplemented!();
-    }
-
-    pub fn clone(&self) -> Self {
-        unimplemented!();
+        let inner = unsafe {
+            &mut *self.inner
+        };
+        inner.vec.capacity()
     }
 
     /// Try to create a read-only `DivBuf` that refers to the entirety of this
@@ -68,11 +66,10 @@ impl DivBufShared {
             inner.refcount.fetch_sub(1, Relaxed);
             None
         } else {
-            let p = inner.vec.as_mut_ptr();
             let l = inner.vec.len();
             Some(DivBuf {
                 inner: self.inner,
-                ptr: p,
+                begin: 0,
                 len: l
             })
         }
@@ -93,12 +90,11 @@ impl DivBufShared {
         let inner = unsafe {
             &mut *self.inner
         };
-        if inner.refcount.compare_and_swap(0, WRITER_FLAG + 1, Acquire) == 0 { 
-            let p = inner.vec.as_mut_ptr();
+        if inner.refcount.compare_and_swap(0, WRITER_FLAG + 1, AcqRel) == 0 { 
             let l = inner.vec.len();
             Some(DivBufMut {
                 inner: self.inner,
-                ptr: p,
+                begin: 0,
                 len: l
             })
         } else {
@@ -106,23 +102,15 @@ impl DivBufShared {
         }
     }
 
-    pub fn from_static(bytes: &'static [u8]) ->  Self {
-        unimplemented!();
-    }
-
     pub fn len(&self) -> usize {
-        unimplemented!();
+        let inner = unsafe {
+            &mut *self.inner
+        };
+        inner.vec.len()
     }
 
     pub fn with_capacity(capacity: usize) -> Self {
-        let mut inner = Box::new(Inner {
-            vec: Vec::with_capacity(capacity),
-            refcount: AtomicUsize::new(0)
-        });
-        let buffer = DivBufShared{
-            inner: Box::into_raw(inner)
-        };
-        buffer
+        Self::from(Vec::with_capacity(capacity))
     }
 }
 
@@ -141,42 +129,60 @@ impl Drop for DivBufShared {
         } else {
             // We don't currently allow dropping a DivBufShared until all of its
             // child DivBufs and DivBufMuts have been dropped, too.
-            panic!("Dropping a DivBufShared that's still referenced: {:?}",
-                   inner.refcount.load(Relaxed));
+            panic!("Dropping a DivBufShared that's still referenced");
         }
     }
 }
 
 impl From<Vec<u8>> for DivBufShared {
     fn from(src: Vec<u8>) -> DivBufShared {
-        unimplemented!();
+        let rc = AtomicUsize::new(0);
+        let mut inner = Box::new(Inner {
+            vec: src,
+            refcount: rc
+        });
+        let buffer = DivBufShared{
+            inner: Box::into_raw(inner)
+        };
+        buffer
     }
 }
-
-impl<'a> From<&'a [u8]> for DivBufShared {
-    fn from(src: &'a [u8]) -> DivBufShared {
-        unimplemented!();
-    }
-}
-
-impl hash::Hash for DivBufShared {
-    fn hash<H>(&self, state: &mut H) where H: hash::Hasher {
-        unimplemented!();
-    }
-}
-
 
 impl DivBuf {
+    /// Returns true if the `DivBuf` has length 0
+    ///
+    /// # Examples
+    /// ```
+    /// # use divbuf::*;
+    ///
+    /// let mut dbs = DivBufShared::with_capacity(64);
+    /// let db0 = dbs.try().unwrap();
+    /// assert!(db0.is_empty());
+    /// ```
     pub fn is_empty(&self) -> bool {
-        unimplemented!();
+        self.len == 0
     }
 
+    /// Get the length of this `DivBuf`, _not_ the underlying storage
     pub fn len(&self) -> usize {
-        unimplemented!();
+        self.len
     }
 
+    /// Create a new DivBuf that spans a subset of this one.
     pub fn slice(&self, begin: usize, end: usize) -> DivBuf {
-        unimplemented!();
+        assert!(begin <= end);
+        assert!(end <= self.len);
+        let inner = unsafe {
+            &mut *self.inner
+        };
+        let old_refcount = inner.refcount.fetch_add(1, Acquire);
+        debug_assert_eq!(old_refcount & WRITER_FLAG, 0);
+        debug_assert!(old_refcount & !WRITER_FLAG > 0);
+        DivBuf {
+            inner: self.inner,
+            begin: self.begin + begin,
+            len: end - begin
+        }
     }    
 
     pub fn slice_from(&self, begin: usize) -> DivBuf {
@@ -187,53 +193,173 @@ impl DivBuf {
         self.slice(0, end)
     }
 
+    /// Splits the DivBuf into two at the given index.
+    ///
+    /// Afterwards self contains elements [0, at), and the returned DivBuf
+    /// contains elements [at, self.len).
+    ///
+    /// This is an O(1) operation
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use divbuf::*;
+    ///
+    /// let mut dbs = DivBufShared::from(vec![1, 2, 3, 4, 5, 6]);
+    /// let mut db0 = dbs.try().unwrap();
+    /// let db1 = db0.split_off(4);
+    /// assert_eq!(db0, [1, 2, 3, 4][..]);
+    /// assert_eq!(db1, [5, 6][..]);
+    /// ```
     pub fn split_off(&mut self, at: usize) -> DivBuf {
-        unimplemented!();
+        assert!(at <= self.len, "Can't split past the end");
+        let inner = unsafe {
+            &mut *self.inner
+        };
+        let old_refcount = inner.refcount.fetch_add(1, Relaxed);
+        debug_assert_eq!(old_refcount & WRITER_FLAG, 0);
+        debug_assert!(old_refcount & !WRITER_FLAG > 0);
+        let right_half = DivBuf {
+            inner: self.inner,
+            begin: self.begin + at,
+            len: self.len - at
+        };
+        self.len = at;
+        right_half
     }
 
+    /// Splits the DivBuf into two at the given index.
+    ///
+    /// Afterwards self contains elements [at, self.len), and the returned
+    /// DivBuf contains elements [0, at).
+    /// This is an O(1) operation.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use divbuf::*;
+    ///
+    /// let mut dbs = DivBufShared::from(vec![1, 2, 3, 4, 5, 6]);
+    /// let mut db0 = dbs.try().unwrap();
+    /// let db1 = db0.split_to(4);
+    /// assert_eq!(db0, [5, 6][..]);
+    /// assert_eq!(db1, [1, 2, 3, 4][..]);
+    /// ```
     pub fn split_to(&mut self, at: usize) -> DivBuf {
-        unimplemented!();
+        assert!(at <= self.len, "Can't split past the end");
+        let inner = unsafe {
+            &mut *self.inner
+        };
+        let old_refcount = inner.refcount.fetch_add(1, Relaxed);
+        debug_assert_eq!(old_refcount & WRITER_FLAG, 0);
+        debug_assert!(old_refcount & !WRITER_FLAG > 0);
+        let left_half = DivBuf {
+            inner: self.inner,
+            begin: self.begin,
+            len: at
+        };
+        self.begin += at;
+        self.len -= at;
+        left_half
     }
 
+    /// Attempt to upgrade Self to a writable DivBufMut
+    ///
+    /// This will fail if there are any other living DivBufs for this same
+    /// DivBufShared
+    ///
+    /// # Examples
+    /// ```
+    /// # use divbuf::*;
+    ///
+    /// let mut dbs = DivBufShared::with_capacity(4096);
+    /// let db = dbs.try().unwrap();
+    /// db.try_mut().unwrap();
+    /// ```
     pub fn try_mut(mut self) -> Result<DivBufMut, DivBuf> {
-        unimplemented!();
+        let inner = unsafe {
+            &mut *self.inner
+        };
+        if inner.refcount.compare_and_swap(1, WRITER_FLAG + 1, AcqRel) == 1 {
+            let mutable_self = Ok(DivBufMut {
+                inner: self.inner,
+                begin: self.begin,
+                len: self.len
+            });
+            mem::forget(self);
+            mutable_self
+        } else {
+            Err(self)
+        }
     }
 
-    pub fn unsplit(&mut self, other: DivBuf) {
-        unimplemented!();
+    /// Combine splitted DivBuf objects back into a contiguous single
+    ///
+    /// If `DivBuf` objects were not contiguous originally, the operation will
+    /// fail and return `other` unmodified
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use divbuf::*;
+    ///
+    /// let mut dbs = DivBufShared::from(vec![1, 2, 3, 4, 5, 6]);
+    /// let mut db0 = dbs.try().unwrap();
+    /// let db1 = db0.split_off(4);
+    /// db0.unsplit(db1);
+    /// assert_eq!(db0, [1, 2, 3, 4, 5, 6][..]);
+    /// ```
+    pub fn unsplit(&mut self, other: DivBuf) -> Result<(), DivBuf> {
+        if self.inner != other.inner {
+            Err(other)
+        } else if (self.begin + self.len) != other.begin {
+            Err(other)
+        } else {
+            self.len += other.len;
+            Ok(())
+        }
     }
 }
 
 impl AsRef<[u8]> for DivBuf {
     fn as_ref(&self) -> &[u8] {
-        unimplemented!();
+        unsafe {
+            let inner = &mut *self.inner;
+            slice::from_raw_parts(&inner.vec[self.begin] as *const u8, self.len)
+        }
     }
 }
 
 impl hash::Hash for DivBuf {
     fn hash<H>(&self, state: &mut H) where H: hash::Hasher {
-        unimplemented!();
+        let s: &[u8] = self.as_ref();
+        s.hash(state);
     }
 }
-
 
 impl ops::Deref for DivBuf {
     type Target = [u8];
 
+    /// TODO: move this doc test to the class or module level
+    ///
+    /// # Examples
+    /// ```compile_fail
+    /// # use divbuf::*;
+    /// let mut dbs = DivBufShared::from(vec![1, 2, 3]);
+    /// let db = dbs.try().unwrap();
+    /// db[0] = 9;
+    /// ```
     fn deref(&self) -> &[u8] {
-        unimplemented!();
-    }
-}
-
-impl From<DivBufMut> for DivBuf {
-    fn from(src: DivBufMut) -> DivBuf {
-        src.freeze()
+        unsafe {
+            let inner = &mut *self.inner;
+            slice::from_raw_parts(&inner.vec[self.begin] as *const u8, self.len)
+        }
     }
 }
 
 impl Clone for DivBuf {
     fn clone(&self) -> DivBuf {
-        unimplemented!();
+        self.slice_from(0)
     }
 }
 
@@ -246,46 +372,215 @@ impl Drop for DivBuf {
     }
 }
 
-impl DivBufMut {
-    pub fn bzero(&mut self) {
-        unimplemented!();
+impl PartialEq for DivBuf {
+    fn eq(&self, other: &DivBuf) -> bool {
+        self.as_ref() == other.as_ref()
     }
+}
 
-    pub fn try_extend<T>(&mut self, iter: T) -> Result<(), &'static str>
-        where T: IntoIterator<Item=u8> {
-        unimplemented!();
+impl PartialEq<[u8]> for DivBuf {
+    fn eq(&self, other: &[u8]) -> bool {
+        self.as_ref() == other
+    }
+}
+
+
+impl DivBufMut {
+    pub fn try_extend<'a, T>(&mut self, iter: T) -> Result<(), &'static str>
+        where T: IntoIterator<Item=&'a u8> {
+        let inner = unsafe {
+            &mut *self.inner
+        };
+        let oldlen = inner.vec.len();
+        if self.begin + self.len != oldlen {
+            Err("Can't extend into the middle of a buffer")
+        } else {
+            self.extend(iter);
+            Ok(())
+        }
     }    
 
-    pub fn freeze(self) -> DivBuf {
-        unimplemented!();
-    }    
-
+    /// Returns true if the `DivBufMut` has length 0
+    ///
+    /// # Examples
+    /// ```
+    /// # use divbuf::*;
+    ///
+    /// let mut dbs = DivBufShared::with_capacity(64);
+    /// let dbm0 = dbs.try_mut().unwrap();
+    /// assert!(dbm0.is_empty());
+    /// ```
     pub fn is_empty(&self) -> bool {
-        unimplemented!();
+        self.len == 0
     }
 
     pub fn len(&self) -> usize {
-        unimplemented!();
+        self.len
     }
 
     pub fn reserve(&mut self, additional: usize) {
-        unimplemented!();
+        let inner = unsafe {
+            &mut *self.inner
+        };
+        inner.vec.reserve(additional)
     }
 
-    pub fn split_off(&mut self, at: usize) -> DivBuf {
-        unimplemented!();
+    /// Splits the DivBufMut into two at the given index.
+    ///
+    /// Afterwards self contains elements [0, at), and the returned DivBufMut
+    /// contains elements [at, self.len).
+    ///
+    /// This is an O(1) operation
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use divbuf::*;
+    ///
+    /// let mut dbs = DivBufShared::from(vec![1, 2, 3, 4, 5, 6]);
+    /// let mut dbm0 = dbs.try_mut().unwrap();
+    /// let dbm1 = dbm0.split_off(4);
+    /// assert_eq!(dbm0, [1, 2, 3, 4][..]);
+    /// assert_eq!(dbm1, [5, 6][..]);
+    /// ```
+    pub fn split_off(&mut self, at: usize) -> DivBufMut {
+        assert!(at <= self.len, "Can't split past the end");
+        let inner = unsafe {
+            &mut *self.inner
+        };
+        let old_refcount = inner.refcount.fetch_add(1, Relaxed);
+        debug_assert_eq!(old_refcount & WRITER_FLAG, WRITER_FLAG);
+        debug_assert!(old_refcount & !WRITER_FLAG > 0);
+        let right_half = DivBufMut {
+            inner: self.inner,
+            begin: self.begin + at,
+            len: self.len - at
+        };
+        self.len = at;
+        right_half
     }
 
-    pub fn split_to(&mut self, at: usize) -> DivBuf {
-        unimplemented!();
+    /// Splits the DivBufMut into two at the given index.
+    ///
+    /// Afterwards self contains elements [at, self.len), and the returned
+    /// DivBufMut contains elements [0, at).
+    /// This is an O(1) operation.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use divbuf::*;
+    ///
+    /// let mut dbs = DivBufShared::from(vec![1, 2, 3, 4, 5, 6]);
+    /// let mut dbm0 = dbs.try_mut().unwrap();
+    /// let dbm1 = dbm0.split_to(4);
+    /// assert_eq!(dbm0, [5, 6][..]);
+    /// assert_eq!(dbm1, [1, 2, 3, 4][..]);
+    /// ```
+    pub fn split_to(&mut self, at: usize) -> DivBufMut {
+        assert!(at <= self.len, "Can't split past the end");
+        let inner = unsafe {
+            &mut *self.inner
+        };
+        let old_refcount = inner.refcount.fetch_add(1, Relaxed);
+        debug_assert_eq!(old_refcount & WRITER_FLAG, WRITER_FLAG);
+        debug_assert!(old_refcount & !WRITER_FLAG > 0);
+        let left_half = DivBufMut {
+            inner: self.inner,
+            begin: self.begin,
+            len: at
+        };
+        self.begin += at;
+        self.len -= at;
+        left_half
     }
 
+    /// Shortens the buffer, keeping the first `len` bytes and dropping the
+    /// rest.
+    ///
+    /// If `len` is greater than the buffer's current length, this has no
+    /// effect.
+    ///
+    /// Will fail if this DivButMut does not include the end of the shared
+    /// vector.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use divbuf::*;
+    ///
+    /// let mut dbs = DivBufShared::from(vec![1, 2, 3, 4, 5, 6]);
+    /// let mut dbm0 = dbs.try_mut().unwrap();
+    /// assert!(dbm0.try_truncate(3).is_ok());
+    /// assert_eq!(dbm0, [1, 2, 3][..]);
+    /// ```
     pub fn try_truncate(&mut self, len: usize) -> Result<(), &'static str> {
-        unimplemented!();
+        let inner = unsafe {
+            &mut *self.inner
+        };
+        if self.begin + self.len != inner.vec.len() {
+            Err("Can't truncate a non-terminal DivBufMut")
+        } else {
+            inner.vec.truncate(self.begin + len);
+            self.len = cmp::min(self.len, len);
+            Ok(())
+        }
     }    
 
-    pub fn unsplit(&mut self, other: DivBufMut) {
-        unimplemented!();
+    /// Combine splitted DivBufMut objects back into a contiguous single
+    ///
+    /// If `DivBufMut` objects were not contiguous originally, the operation
+    /// will fail and return `other` unmodified
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use divbuf::*;
+    ///
+    /// let mut dbs = DivBufShared::from(vec![1, 2, 3, 4, 5, 6]);
+    /// let mut dbm0 = dbs.try_mut().unwrap();
+    /// let dbm1 = dbm0.split_off(4);
+    /// dbm0.unsplit(dbm1);
+    /// assert_eq!(dbm0, [1, 2, 3, 4, 5, 6][..]);
+    /// ```
+    pub fn unsplit(&mut self, other: DivBufMut) -> Result<(), DivBufMut> {
+        if self.inner != other.inner {
+            Err(other)
+        } else if (self.begin + self.len) != other.begin {
+            Err(other)
+        } else {
+            self.len += other.len;
+            Ok(())
+        }
+    }
+}
+
+impl AsRef<[u8]> for DivBufMut {
+    fn as_ref(&self) -> &[u8] {
+        unsafe {
+            let inner = &mut *self.inner;
+            slice::from_raw_parts(&inner.vec[self.begin] as *const u8, self.len)
+        }
+    }
+}
+
+impl ops::Deref for DivBufMut {
+    type Target = [u8];
+
+    fn deref(&self) -> &[u8] {
+        unsafe {
+            let inner = &mut *self.inner;
+            slice::from_raw_parts(&inner.vec[self.begin] as *const u8, self.len)
+        }
+    }
+}
+
+impl ops::DerefMut for DivBufMut {
+    fn deref_mut(&mut self) -> &mut [u8] {
+        unsafe {
+            let inner = &mut *self.inner;
+            slice::from_raw_parts_mut(&mut inner.vec[self.begin] as *mut u8, self.len)
+        }
     }
 }
 
@@ -316,17 +611,39 @@ impl Drop for DivBufMut {
     }
 }
 
-impl Extend<u8> for DivBufMut {
+impl<'a> Extend<&'a u8> for DivBufMut {
     fn extend<T>(&mut self, iter: T)
-        where T: IntoIterator<Item=u8> {
+        where T: IntoIterator<Item = &'a u8> {
+        let inner = unsafe {
+            &mut *self.inner
+        };
         // panic if this DivBufMut does not extend to the end of the
         // DivBufShared
-        unimplemented!();
+        let oldlen = inner.vec.len();
+        assert_eq!(self.begin + self.len, oldlen,
+            "Can't extend into the middle of a buffer");
+        inner.vec.extend(iter);
+        self.len += inner.vec.len() - oldlen;
     }
 }
 
 impl hash::Hash for DivBufMut {
     fn hash<H>(&self, state: &mut H) where H: hash::Hasher {
-        unimplemented!();
+        let s: &[u8] = self.as_ref();
+        s.hash(state);
     }
 }
+
+impl PartialEq for DivBufMut {
+    fn eq(&self, other: &DivBufMut) -> bool {
+        self.as_ref() == other.as_ref()
+    }
+}
+
+impl PartialEq<[u8]> for DivBufMut {
+    fn eq(&self, other: &[u8]) -> bool {
+        self.as_ref() == other
+    }
+}
+
+
--- tests/test.rs
+++ tests/test.rs
@@ -1,7 +1,14 @@
+// vim: tw=80
 extern crate divbuf;
+
+use std::collections::hash_map::DefaultHasher;
+use std::hash::{Hash, Hasher};
 
 use divbuf::*;
 
+//
+// DivBufShared methods
+//
 #[test]
 pub fn test_divbufshared_caplen() {
     let mut v = Vec::<u8>::with_capacity(64);
@@ -47,17 +54,18 @@ pub fn test_divbufshared_try_mut() {
     assert!(dbs.try_mut().is_none());
 }
 
+//
+// DivBuf methods
+//
 #[test]
-pub fn test_divbufmut_extend() {
+pub fn test_divbuf_clone() {
     let mut dbs = DivBufShared::from(vec![1, 2, 3]);
-    {
-        let mut dbm = dbs.try_mut().unwrap();
-        dbm.extend([4, 5, 6].iter());
-    }
-    // verify that dbs.inner.vec was extended
-    let db = dbs.try().unwrap();
-    let slice : &[u8] = &db;
-    assert_eq!(slice, &[1, 2, 3, 4, 5, 6]);
+    let db0 = dbs.try().unwrap();
+    let mut db1 = db0.clone();
+    assert_eq!(db0, db1);
+    // We should be able to modify one DivBuf without affecting the other
+    db1.split_off(1);
+    assert_ne!(db0, db1);
 }
 
 #[test]
@@ -78,6 +86,32 @@ pub fn test_divbuf_eq() {
     let db2 = dbs2.try().unwrap();
     assert_eq!(db0, db1);
     assert_ne!(db0, db2);
+}
+
+#[test]
+pub fn test_divbuf_is_empty() {
+    let mut dbs0 = DivBufShared::with_capacity(64);
+    let db0 = dbs0.try().unwrap();
+    assert!(db0.is_empty());
+
+    let mut dbs1 = DivBufShared::from(vec![1]);
+    let db1 = dbs1.try().unwrap();
+    assert!(!db1.is_empty());
+}
+
+fn simple_hash<T: Hash>(t: &T) -> u64 {
+    let mut s = DefaultHasher::new();
+    t.hash(&mut s);
+    s.finish()
+}
+
+#[test]
+pub fn test_divbuf_hash() {
+    let v = vec![1, 2, 3, 4, 5, 6];
+    let expected = simple_hash(&v);
+    let mut dbs = DivBufShared::from(v);
+    let db0 = dbs.try().unwrap();
+    assert_eq!(simple_hash(&db0), expected);
 }
 
 #[test]
@@ -198,8 +232,8 @@ pub fn test_divbuf_trymut() {
 
 #[test]
 pub fn test_divbuf_unsplit() {
-    let mut dbs = DivBufShared::from(vec![1, 2, 3, 4, 5, 6]);
-    let mut db0 = dbs.try().unwrap();
+    let mut dbs0 = DivBufShared::from(vec![1, 2, 3, 4, 5, 6]);
+    let mut db0 = dbs0.try().unwrap();
     {
         // split in the middle
         let db_mid = db0.split_off(4);
@@ -221,12 +255,224 @@ pub fn test_divbuf_unsplit() {
         let db_end = db0.slice_from(2);
         assert!(db_begin.unsplit(db_end).is_err());
     }
+
+    {
+        // unsplit should fail for unrelated DivBufs
+        let mut dbs1 = DivBufShared::from(vec![7, 8, 9]);
+        let mut db_end = db0.slice_from(4);
+        let db_unrelated = dbs1.try().unwrap();
+        assert!(db_end.unsplit(db_unrelated).is_err());
+    }
 }
 
+//
+// DivBufMut methods
+//
 #[test]
 pub fn test_divbufmut_deref() {
     let mut dbs = DivBufShared::from(vec![1, 2, 3]);
     let dbm = dbs.try_mut().unwrap();
     let slice : &[u8] = &dbm;
     assert_eq!(slice, &[1, 2, 3]);
+}
+
+#[test]
+pub fn test_divbufmut_derefmut() {
+    let mut dbs = DivBufShared::from(vec![1, 2, 3]);
+    let mut dbm = dbs.try_mut().unwrap();
+    // Unlike DivBuf, we _can_ update DivBufMuts randomly
+    dbm[0] = 9;
+    let slice : &mut [u8] = &mut dbm;
+    assert_eq!(slice, &[9, 2, 3]);
+}
+
+#[test]
+pub fn test_divbufmut_eq() {
+    let mut dbs0 = DivBufShared::from(vec![1, 2, 3]);
+    let mut dbs1 = DivBufShared::from(vec![1, 2, 3]);
+    let mut dbs2 = DivBufShared::from(vec![1, 2]);
+    let dbm0 = dbs0.try_mut().unwrap();
+    let dbm1 = dbs1.try_mut().unwrap();
+    let dbm2 = dbs2.try_mut().unwrap();
+    assert_eq!(dbm0, dbm1);
+    assert_ne!(dbm0, dbm2);
+}
+
+#[test]
+pub fn test_divbufmut_extend() {
+    let mut dbs = DivBufShared::from(vec![1, 2, 3]);
+    {
+        let mut dbm = dbs.try_mut().unwrap();
+        dbm.extend([4, 5, 6].iter());
+    }
+    // verify that dbs.inner.vec was extended
+    let db = dbs.try().unwrap();
+    let slice : &[u8] = &db;
+    assert_eq!(slice, &[1, 2, 3, 4, 5, 6]);
+}
+
+#[test]
+#[should_panic(expected = "extend into the middle of a buffer")]
+pub fn test_divbufmut_extend_from_the_middle() {
+    let mut dbs = DivBufShared::from(vec![1, 2, 3, 4, 5, 6]);
+    let mut dbm = dbs.try_mut().unwrap();
+    let mut dbm_begin = dbm.split_to(3);
+    dbm_begin.extend([7, 8, 9].iter());
+}
+
+#[test]
+pub fn test_divbufmut_hash() {
+    let v = vec![1, 2, 3, 4, 5, 6];
+    let expected = simple_hash(&v);
+    let mut dbs = DivBufShared::from(v);
+    let dbm0 = dbs.try_mut().unwrap();
+    assert_eq!(simple_hash(&dbm0), expected);
+}
+
+#[test]
+pub fn test_divbufmut_is_empty() {
+    let mut dbs0 = DivBufShared::with_capacity(64);
+    let mut dbm0 = dbs0.try_mut().unwrap();
+    assert!(dbm0.is_empty());
+
+    dbm0.extend([4, 5, 6].iter());
+    assert!(!dbm0.is_empty());
+}
+
+#[test]
+pub fn test_divbufmut_reserve() {
+    let v = Vec::<u8>::with_capacity(64);
+    let mut dbs = DivBufShared::from(v);
+    let mut dbm = dbs.try_mut().unwrap();
+    dbm.reserve(128);
+    assert_eq!(dbs.capacity(), 128);
+}
+
+#[test]
+pub fn test_divbufmut_split_off() {
+    let mut dbs = DivBufShared::from(vec![1, 2, 3, 4, 5, 6]);
+    let mut dbm0 = dbs.try_mut().unwrap();
+    // split in the middle
+    let dbm_mid = dbm0.split_off(4);
+    assert_eq!(dbm0, [1, 2, 3, 4][..]);
+    assert_eq!(dbm0.len(), 4);
+    assert_eq!(dbm_mid, [5, 6][..]);
+    assert_eq!(dbm_mid.len(), 2);
+    // split at the beginning
+    let mut dbm_begin = dbm0.split_off(0);
+    assert_eq!(dbm0, [][..]);
+    assert_eq!(dbm_begin, [1, 2, 3, 4][..]);
+    // split at the end
+    let dbm_end = dbm_begin.split_off(4);
+    assert_eq!(dbm_begin, [1, 2, 3, 4][..]);
+    assert_eq!(dbm_end, [][..]);
+}
+
+#[test]
+#[should_panic(expected = "Can't split past the end")]
+pub fn test_divbufmut_split_off_past_the_end() {
+    let mut dbs = DivBufShared::from(vec![1, 2, 3, 4, 5, 6]);
+    let mut dbm0 = dbs.try_mut().unwrap();
+    dbm0.split_off(7);
+}
+
+#[test]
+pub fn test_divbufmut_split_to() {
+    let mut dbs = DivBufShared::from(vec![1, 2, 3, 4, 5, 6]);
+    let mut dbm0 = dbs.try_mut().unwrap();
+    // split in the middle
+    let mut dbm_left = dbm0.split_to(4);
+    assert_eq!(dbm_left, [1, 2, 3, 4][..]);
+    assert_eq!(dbm_left.len(), 4);
+    assert_eq!(dbm0, [5, 6][..]);
+    assert_eq!(dbm0.len(), 2);
+    // split at the beginning
+    let dbm_begin = dbm_left.split_to(0);
+    assert_eq!(dbm_begin, [][..]);
+    assert_eq!(dbm_left, [1, 2, 3, 4][..]);
+    // split at the end
+    let dbm_mid = dbm_left.split_to(4);
+    assert_eq!(dbm_mid, [1, 2, 3, 4][..]);
+    assert_eq!(dbm_left, [][..]);
+}
+
+#[test]
+#[should_panic(expected = "Can't split past the end")]
+pub fn test_divbufmut_split_to_past_the_end() {
+    let mut dbs = DivBufShared::from(vec![1, 2, 3, 4, 5, 6]);
+    let mut dbm0 = dbs.try_mut().unwrap();
+    dbm0.split_to(7);
+}
+
+#[test]
+pub fn test_divbufmut_try_extend() {
+    let mut dbs = DivBufShared::from(vec![1, 2, 3]);
+    {
+        let mut dbm0 = dbs.try_mut().unwrap();
+        assert!(dbm0.try_extend([4, 5, 6].iter()).is_ok());
+
+        // Extending from the middle of the vec should fail
+        let mut dbm1 = dbm0.split_to(2);
+        assert!(dbm1.try_extend([7, 8, 9].iter()).is_err());
+    }
+
+    // verify that dbs.inner.vec was extended the first time, but not the
+    // second.
+    let db = dbs.try().unwrap();
+    assert_eq!(db, [1, 2, 3, 4, 5, 6][..]);
+}
+
+#[test]
+pub fn test_divbufmut_try_truncate() {
+    let mut dbs = DivBufShared::from(vec![1, 2, 3, 4, 5, 6]);
+    {
+        let mut dbm0 = dbs.try_mut().unwrap();
+        // First, truncate past the end of the vector
+        assert!(dbm0.try_truncate(7).is_ok());
+        assert_eq!(dbm0.len(), 6);
+        // Then, do a normal truncation
+        assert!(dbm0.try_truncate(4).is_ok());
+        assert_eq!(dbm0, [1, 2, 3, 4][..]);
+        // Check that the shared vector was truncated, too
+        assert_eq!(dbs.len(), 4);
+        // A truncation of a non-terminal DivBufMut should fail
+        let mut dbm1 = dbm0.split_to(2);
+        assert!(dbm1.try_truncate(3).is_err());
+        assert_eq!(dbs.len(), 4);
+        // Truncating a terminal DivBufMut should work, even if it doesn't start
+        // at the vector's beginning
+        assert!(dbm0.try_truncate(1).is_ok());
+        assert_eq!(dbs.len(), 3);
+
+    }
+}
+
+#[test]
+pub fn test_divbufmut_unsplit() {
+    let mut dbs0 = DivBufShared::from(vec![1, 2, 3, 4, 5, 6]);
+    {
+        let mut dbm0 = dbs0.try_mut().unwrap();
+        // split in the middle
+        let dbm_mid = dbm0.split_off(4);
+        // put it back together
+        assert!(dbm0.unsplit(dbm_mid).is_ok());
+        assert_eq!(dbm0, [1, 2, 3, 4, 5, 6][..]);
+    }
+
+    {
+        // unsplit should fail for noncontiguous DivBufMuts
+        let mut dbm0 = dbs0.try_mut().unwrap();
+        let mut dbm_begin = dbm0.split_to(2);
+        let dbm_end = dbm0.split_off(2);
+        assert!(dbm_begin.unsplit(dbm_end).is_err());
+    }
+
+    {
+        // unsplit should fail for unrelated DivBufMuts
+        let mut dbm0 = dbs0.try_mut().unwrap();
+        let mut dbs1 = DivBufShared::from(vec![7, 8, 9]);
+        let mut dbm_end = dbm0.split_off(4);
+        let dbm_unrelated = dbs1.try_mut().unwrap();
+        assert!(dbm_end.unsplit(dbm_unrelated).is_err());
+    }
 }
